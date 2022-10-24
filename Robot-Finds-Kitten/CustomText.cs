db143c2e3f8d85bb74d9cb1238b0cfc1ca0eb88f
Michael Cabatingan <mikecabatingan@icloud.com>
2022-10-24 16:34:57 -0600

    Minor Changes

--- Robot-Finds-Kitten/CustomText.cs
+++ Robot-Finds-Kitten/CustomText.cs
@@ -14,18 +14,18 @@ namespace RFK
         private float end_time;
         private float time;
         private float current_time;
-        private int fontSize;
+        private int font_size;
         private Color color;
         private SceneHandler scene_handler;
         private Vector2 position;
-        public AnimatedText(string text, float start_time, float end_time, Vector2 position, int fontSize, Color color, SceneHandler scene_handler)
+        public AnimatedText(string text, float start_time, float end_time, Vector2 position, int font_size, Color color, SceneHandler scene_handler)
         {
             this.text = text;
             this.start_time = start_time;
             this.end_time = end_time;
             this.time = Math.Abs(end_time - start_time);
             this.position = position;
-            this.fontSize = fontSize;
+            this.font_size = font_size;
             this.color = color;
 
             this.characters_printed = 0;
@@ -62,7 +62,7 @@ namespace RFK
         }
         public void draw()
         {
-            int line_characters = (int) Math.Ceiling((float)(this.scene_handler.getSize().X - this.position.X) / (float) this.fontSize);
+            int line_characters = (int) Math.Ceiling((float)(this.scene_handler.getSize().X - this.position.X) / (float) this.font_size);
             // I should maybe get this to work with MeasureText instead.
             line_characters *= 2;
             int sub_start = 0;
@@ -80,9 +80,9 @@ namespace RFK
                 {
                     text_length = this.characters_printed - sub_start;
                 }
-                DrawText(this.text.Substring(sub_start, text_length), (int) (this.position.X + offset.X), (int) (this.position.Y + offset.Y), this.fontSize, this.color);
+                DrawText(this.text.Substring(sub_start, text_length), (int) (this.position.X + offset.X), (int) (this.position.Y + offset.Y), this.font_size, this.color);
                 sub_start += line_characters;
-                offset.Y += this.fontSize;
+                offset.Y += this.font_size;
             }
         }
     }
